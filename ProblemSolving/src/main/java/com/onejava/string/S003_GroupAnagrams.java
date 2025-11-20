88b91715dd883ebf5a78a5b125cfe1d6fad9ee66
vivek <vyk0522@gmail.com>
2025-11-20 16:58:34 +0530

    Added sorting

--- ProblemSolving/src/main/java/com/onejava/string/S003_GroupAnagrams.java
+++ ProblemSolving/src/main/java/com/onejava/string/S003_GroupAnagrams.java
@@ -3,18 +3,28 @@ package com.onejava.string;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/*
+        Group Anagrams and sort the list
+ */
 public class S003_GroupAnagrams {
     public static void main(String[] args) {
         String[] words = {"eat","tea","tan","ate","nat","bat"};
         List<List<String>> lists = groupAnagramsStream(words);
-        System.out.println(lists); // [[eat, tea, ate], [bat], [tan, nat]]
+        System.out.println(lists); // [[ate, eat, tea], [bat], [nat, tan]] // sorted list
 
         List<List<String>> lists2 = groupAnagramsMap(words);
         System.out.println(lists2); // [[eat, tea, ate], [bat], [tan, nat]]
     }
 
     private static List<List<String>> groupAnagramsStream(String[] words){
-        return Arrays.stream(words).collect(Collectors.groupingBy(S003_GroupAnagrams::sortedWord)).values().stream().toList();
+        Map<String, List<String>> listMap = Arrays.stream(words)
+                .collect(Collectors.groupingBy(S003_GroupAnagrams::sortedWord));
+
+        List<List<String>> anagramsList = listMap.values().stream().toList();
+
+        anagramsList.forEach(anagrams -> Collections.sort(anagrams));
+
+        return anagramsList;
     }
 
     private static List<List<String>> groupAnagramsMap(String[] words){
